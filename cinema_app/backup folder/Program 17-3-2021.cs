7ec1eff4f38332988a8b7ac9519eda127872004d
BR1005978 <1005978@hr.nl>
2021-03-24 12:43:27 +0100

    eerste commit. ik gooi gewoon alles wat ik nu heb op de git. misschien werkt het, misschien ook niet

---
+++ cinema_app/backup folder/Program 17-3-2021.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cinema_app
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            void WelcomeScreen()
+            {
+                // void GuestWelcome()
+                Console.WriteLine("Welcome to Fictorama! \n" +
+                    "This is Fictoram Interface 0.02\n\n");
+
+                List<string> options = new List<string> { "1", "2", "3", "4" };
+                string answer = "";
+                while (!options.Contains(answer))
+                {
+
+                    Console.WriteLine("Please choose what you want to do: \n" +
+                    "1. See available movies\n" +
+                    "2. Login\n" +
+                    "3. Register\n" +
+                    "4. Exit program\n"
+                    );
+                    answer = Console.ReadLine();
+
+
+                    if (answer == "1")
+                    {
+                        //Console.WriteLine("You picked \"1. See available movies\" \n" +
+                        //    "WORK IN PROGRESS - Please come back later");
+                        MovieBrowser();
+
+                    }
+                    else if (answer == "2")
+                    {
+                        //Console.WriteLine("You picked \"2. Login\" \n" +
+                        //    "WORK IN PROGRESS - Please come back later");
+                        LoginScreen();
+
+                    }
+                    else if (answer == "3")
+                    {
+                        //Console.WriteLine("You picked \"3. Register\" \n" +
+                        //    "WORK IN PROGRESS - Please come back later");
+                        RegisterScreen();
+
+                    }
+                    else if (answer == "4")
+                    {
+                        ExitScreen();
+                    }
+                    else
+                    { Console.WriteLine("Your input was: " + answer + "\nInput not recognised. Please try again\n"); }
+                }
+                Console.WriteLine("Shutting down ...");
+            }
+
+
+            void MovieBrowser()
+            {
+                Console.WriteLine("You picked \"1. See available movies\" \n");
+
+            }
+            void LoginScreen()
+            {
+                Console.WriteLine("You picked \"2. Login\" \n" +
+                        "WORK IN PROGRESS - Please come back later");
+            }
+
+            void RegisterScreen()
+            {
+                Console.WriteLine("You picked \"3. Register\" \n" +
+                    "WORK IN PROGRESS - Please come back later");
+            }
+            void ExitScreen()
+            {
+                Console.WriteLine("Shutting down...");
+            }
+
+            WelcomeScreen();
+        }
+    }
+}

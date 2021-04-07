add596ab38d975944dcf94dc1525ac9567277f90
BR1005978 <1005978@hr.nl>
2021-04-07 10:08:23 +0200

    verdere aanpassingen, wat quality-of-life dingetjes toegevoegd, taalfouten gecorrigeerd, etc

--- cinema_app/Main_File.cs
+++ cinema_app/Main_File.cs
@@ -6,12 +6,6 @@ namespace cinema_app
 
     class MainProgram
     {
-        //dit is een handige hulpfunctie die je kunt gebruiken om op de python manier
-        //iets te printen. gewoon print("hallo") bijvoorbeeld
-        public static void print(string s)
-        {
-            Console.WriteLine(s);
-        }
 
 
         public static void MainMenu()
@@ -22,19 +16,13 @@ namespace cinema_app
 "▒█░░░ ▄█▄ ▒█▄▄█ ░▒█░░ ▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█░░▒█ ▒█░▒█ \n");
             Console.WriteLine("\nWelcome to Fictorama! \n" +
                 "This is Fictoram Interface 0.03\n\n");
-            //if (UserCheck){
-            //    Console.WriteLine("Usercheck is true");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Usercheck is false");
-            //}
+
             List<string> options = new List<string> { "1", "2", "3", "4", "5" };
             string answer = "";
             while (!options.Contains(answer))
             {
 
-                Console.WriteLine("Please choose what you want to do: \n" +
+                Console.WriteLine("Please choose what you want to do (type the numbers): \n" +
                 "1. See available movies\n" +
                 "2. Login\n" +
                 "3. Register\n" +
@@ -70,10 +58,7 @@ namespace cinema_app
                 {
                     Console.WriteLine("Shutting down...");
                 }
-                else if (answer == "admin")
-                {
-                    Admin.AdminPanel();
-                }
+
                 else
                 {
                     { Console.WriteLine("Your input was: " + answer + "\nInput not recognised. Please try again\n"); }

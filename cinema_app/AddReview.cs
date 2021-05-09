3746c93bffe8aab7f198d323c0ed1711fe70eaab
Prisonig <younessimouh@hotmail.com>
2021-05-10 01:22:15 +0200

    Added review function

---
+++ cinema_app/AddReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace cinema_app
+{
+    public class AddReview
+    {
+        public static void Addreview(string review, User user, Movie movie, DateTime date) {
+            Tuple<string, DateTime, User> Review = Tuple.Create(review, date, user);
+            movie.review.Add(Review);
+        }
+
+
+    }
+}
+
